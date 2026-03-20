d2b588248f18f4739255327ffb2ec5a2049ee38e
Shravan <shravan@techotlist.com>
2026-03-20 09:23:11 +0530

    feat: add tablet (810px) and desktop (1280px) custom Tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      screens: {
+        tablet: '810px',
+        desktop: '1280px',
+      },
+    },
+  },
   plugins: [],
 }
